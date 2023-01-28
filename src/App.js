5f8fcf31ab16d9a1937248c55ff03f01583aaaef
Sinan Sinan <18sos1@queensu.ca>
2023-01-28 08:29:23 -0500

    Making Login

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import { Routes, Route } from "react-router-dom"
+import Layout from './components/layout'
+import SignUp from './components/signup'
+import LogIn from './components/login'
+import Home from "./components/home"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <>
+      <Routes>
+        <Route path='/' element={<Layout />}>
+          <Route path="/" element={<Home />} />
+          <Route path='/login' element={<LogIn />} />
+          <Route path='/signup' element={<SignUp />} />
+        </Route>
+      </Routes>
+   </>
   );
 }
 
---
+++ src/appwrite.js
@@ -0,0 +1,8 @@
+import { Client } from 'appwrite';
+
+const client = new Client();
+
+client
+    .setEndpoint('https://qhacks2023.sinos.work/v1')
+    .setProject('63d490f83f2e3179a8a3');
+
