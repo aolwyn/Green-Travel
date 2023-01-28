bf413ac6fc222f0a86cfe00b0df24455b34aab3a
19ahb <19ahb@queensu.ca>
2023-01-28 14:59:49 -0500

    Directions Setup

--- src/components/home.js
+++ src/components/home.js
@@ -1,7 +1,53 @@
 import React from "react";
 import './myStyles.css';
-import { GoogleMap, useJsApiLoader } from '@react-google-maps/api';
+import { Autocomplete, GoogleMap, useJsApiLoader } from '@react-google-maps/api';
 import Map from "./Map";
+
+
+async function calculateRoute() {
+    if (originRef.current.value === '' || destinationRef.current.value === '') {
+      return
+    }
+    // eslint-disable-next-line no-undef
+    const directionsService = new google.maps.DirectionsService()
+    const results = await directionsService.route({
+      origin: originRef.current.value,
+      destination: destinationRef.current.value,
+      // eslint-disable-next-line no-undef
+      travelMode: google.maps.TravelMode.DRIVING,
+    })
+    setDirectionsResponse(results)
+    setDistance(results.routes[0].legs[0].distance.text)
+    setDuration(results.routes[0].legs[0].duration.text)
+  }
+
+  function clearRoute() {
+    setDirectionsResponse(null)
+    setDistance('')
+    setDuration('')
+    originRef.current.value = ''
+    destinationRef.current.value = ''
+  }
+
+  directionsService.route(
+    {
+      origin: origin,
+      destination: destination,
+      travelMode: google.maps.TravelMode.DRIVING
+    },
+    (result, status) => {
+      if (status === google.maps.DirectionsStatus.OK) {
+        this.setState({
+          directions: result
+        });
+      } else {
+        console.error(`error fetching directions ${result}`);
+      }
+    }
+  );
+
+
+
 
 
 const Home = ({ user, dispatch }) => {
@@ -14,10 +60,13 @@ const Home = ({ user, dispatch }) => {
                 <div class="body">
 
                     <div class="left">
-
                         <form>
-                            <input type="text" name="from" placeholder="From..."/>
-                            <input type="text" name="to" placeholder="To..."/>
+                            <Autocomplete>
+                            <input type="text" ref  ={originRef} name="from" placeholder="From..."/>
+                            </Autocomplete>
+                            <Autocomplete>
+                            <input type="text" ref = {destinationRef} name="to" placeholder="To..."/>
+                            </Autocomplete>
                         </form>
 
                         <div class="statsDisplay">
