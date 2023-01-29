05f615db319303b50166e9fcd511db9af29a5889
Sinan Sinan <18sos1@queensu.ca>
2023-01-28 20:44:38 -0500

    Directions

--- src/components/home.js
+++ src/components/home.js
@@ -3,86 +3,58 @@ import './myStyles.css';
 import { Autocomplete, GoogleMap, useJsApiLoader, DirectionsRenderer } from '@react-google-maps/api';
 import Map from "./Map";
 import axios from "axios";
-var config = {
-    method: 'get',
-    url: '',
-    headers: { 
-        post: {
-            "Access-Control-Allow-Origin": "*",
-            "Content-Type": "application/json"
-        } 
-    }
+
+const config = {
+  method: 'get',
+  url: '',
+  headers: { 
+      post: {
+          "Access-Control-Allow-Origin": "*",
+          "Content-Type": "application/json"
+      } 
+  }
 };
+
+
 const Home = ({ user, dispatch }) => {
-    const [map, setMap] = useState(/** @type google.maps.Map */ (null))
-    const [directionsResponse, setDirectionsResponse] = useState(null)
+
+    const [origin, setOrigin] = useState('')
+    const [destination, setDestination] = useState('')
     const [distance, setDistance] = useState('')
     const [duration, setDuration] = useState('')
-    const [url, setUrl] = useState('')
     const [data, setData] = useState('')
-    /** @type React.MutableRefObject<HTMLInputElement> */
-    const originRef = useRef()
-    /** @type React.MutableRefObject<HTMLInputElement> */
-    const destinationRef = useRef()
-    const google = window.google;    
-    const fetchData = React.useEffect(async () => {
-        await axios(config).then((res) => {
-            setData(res.data)
-        })
-    }, [url])
 
-    useEffect(async() => {
-        await fetchData()
-    }, [fetchData])
+    const handleOrigin = async (e) => {
+        setOrigin(e.target.value)
+    }
+
+    const handleDestination = async (e) => {
+        setDestination(e.target.value)
+    }
+
     async function calculateRoute() {
-        if (originRef.current.value === '' || destinationRef.current.value === '') {
+        if (origin === '' || destination === '') {
             return
-
         }
         
-        let url = encodeURI(`https://maps.googleapis.com/maps/api/directions/json?origin=${originRef.current.value}&destination=${destinationRef.current.value}&key=${process.env.REACT_APP_GOOGLE_MAPS_API_KEY}`);
-        setUrl(url)
-        console.log(data)
-        setDistance(data[0].legs[0].distance.value)
-        setDuration(data[0].legs[0].duration.value)
-    }
-    
-    function clearRoute() {
-        setDirectionsResponse(null)
-        setDistance('')
-        setDuration('')
-        originRef.current.value = ''
-        destinationRef.current.value = ''
-    }
-    
-    // directionsService.route(
-    //     {
-    //         origin: origin,
-    //         destination: destination,
-    //         travelMode: google.maps.TravelMode.DRIVING
-    //     },
-    //     (result, status) => {
-    //         if (status === google.maps.DirectionsStatus.OK) {
-    //         this.setState({
-    //             directions: result
-    //         });
-    //         } else {
-    //         console.error(`error fetching directions ${result}`);
-    //         }
-    //     }
-    // );
+        let url = encodeURI(`https://maps.googleapis.com/maps/api/directions/json?origin=${origin}&destination=${destination}&key=${process.env.REACT_APP_GOOGLE_MAPS_API_KEY}`);
+        config.url = url
+        let data = await axios.get(config.url, config.headers)
 
+        setData(data.data.routes)
+        setDistance(data.data.routes[0].legs[0].distance.value)
+        setDuration(data.data.routes[0].legs[0].duration.value)
+    }
+  
     return(
         <>
             <div className="container">
                 <h1>App Name</h1>
                 <div className="body">
                     <div className="left">
-                       
-                        <input type="text" ref={originRef} name="from" placeholder="Choose a starting point"/>
-                        <input type="text" ref={destinationRef} name="to" placeholder="Choose a destination"/>
+                        <input type="text" onChange={handleOrigin} name="from" placeholder="Choose a starting point"/>
+                        <input type="text" onChange={handleDestination} name="to" placeholder="Choose a destination"/>
                         <button onClick={calculateRoute}>Submit</button>
-                        <p>{ distance }</p>
                         <div className="statsDisplay">
                             Info about saving 
                         </div>
